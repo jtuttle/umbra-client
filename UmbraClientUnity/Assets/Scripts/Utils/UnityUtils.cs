5d1b703e397f549fdf6a9be4d51c765e8faa2d47
john <marlfox@gmail.com>
2013-09-14 14:20:55 -0400

    added playerview and controls

--- UmbraClientUnity/Assets/Scripts/Utils/UnityUtils.cs
+++ UmbraClientUnity/Assets/Scripts/Utils/UnityUtils.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using System;
 
 public class UnityUtils {
-    public static T LoadResource<T>(string path, bool instantiate = false) where T : Object {
-        T resource = (T)Resources.Load(path, typeof(T));
-        return (instantiate ? (T)GameObject.Instantiate(resource) : resource);
+    public static T LoadResource<T>(string path, bool instantiate = false) where T : UnityEngine.Object {
+        try {
+            T resource = (T)Resources.Load(path, typeof(T));
+            return (instantiate ? (T)GameObject.Instantiate(resource) : resource);
+        } catch(Exception) {
+            throw new ArgumentException("Unable to load resource: " + path);
+        }
     }
 }
