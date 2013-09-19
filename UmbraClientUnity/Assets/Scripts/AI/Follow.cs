5b5e743497d8f7533b25b075507246e5433102d8
John Tuttle <jtuttle@craw.cc>
2013-09-19 08:24:46 -0400

    pretty good knockback of enemies

--- UmbraClientUnity/Assets/Scripts/AI/Follow.cs
+++ UmbraClientUnity/Assets/Scripts/AI/Follow.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Holoville.HOTween;
 
 public class Follow : MonoBehaviour {
     public Transform Target;
-    
-    private float _velocityMax = 10.0f;
-    private float _acceleration = 0.1f;
 
-    private float Speed;
+    private float _velocity;
+    private float _velocityMax = 40.0f;
+    private float _acceleration;
+    private float _accelerationMax = 10.0f;
 
     protected void Awake() {
-        Speed = 10.0f;
+        _velocity = _velocityMax;
+        _acceleration = _accelerationMax;
     }
 
 	protected void Update () {
-        float angle = AngleToTarget();
-        float dx = Mathf.Cos(angle) * Speed;
-        float dy = Mathf.Sin(angle) * Speed;
+        _acceleration = Mathf.Min(_acceleration + 2.0f, _accelerationMax);
+        _velocity = Mathf.Min(_velocity + _acceleration, _velocityMax);
 
-        rigidbody.velocity = new Vector3(dx * Speed, dy * Speed, 0);
+        float angle = AngleToTarget();
+        float vx = Mathf.Cos(angle) * _velocity;
+        float vy = Mathf.Sin(angle) * _velocity;
+
+        rigidbody.velocity = new Vector3(vx, vy, 0);
 	}
 
     private float AngleToTarget() {
@@ -26,4 +31,10 @@ public class Follow : MonoBehaviour {
         float distY = Target.position.y - gameObject.transform.position.y;
         return Mathf.Atan2(distY, distX);
     }   
+
+    // TODO: the logic for controlling an enemy's reaction should be on a separate component
+    public void React() {
+        _acceleration = 0;
+        _velocity = -400.0f;
+    }
 }
--- UmbraClientUnity/Assets/Scripts/View/AttackCollider.cs
+++ UmbraClientUnity/Assets/Scripts/View/AttackCollider.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class AttackCollider : MonoBehaviour {
     protected void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Enemy") {
-            Vector3 force = new Vector3(1000, 0, 0);
+            //Vector3 force = new Vector3(1000, 0, 0);
 
-            other.gameObject.rigidbody.AddForce(force);
+            other.gameObject.GetComponent<Follow>().React();
         }
     }
 }
