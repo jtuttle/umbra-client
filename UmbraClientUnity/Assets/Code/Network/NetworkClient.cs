a2a2cf3a83cffe2d8469c55cbf38264d3a38b415
marlfox130 <jtuttle@craw.cc>
2014-02-01 19:17:23 -0500

    ulink plugin, server/client connection, level setup, player instantiation

---
+++ UmbraClientUnity/Assets/Code/Network/NetworkClient.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkClient : UnityEngine.MonoBehaviour {
+    public string ServerIp = "127.0.0.1";
+    public int ServerPort = 7100;
+
+    protected void Awake() {
+        uLink.Network.isAuthoritativeServer = true;
+        uLink.Network.Connect(ServerIp, ServerPort);
+    }
+
+    protected void uLink_OnConnectedToServer() {
+        Debug.Log("Connected to server on port: " + uLink.Network.player.port.ToString());
+
+        int levelSeed = (int)uLink.Network.approvalData.ReadObject(typeof(int).TypeHandle);
+        Debug.Log("Retrieved level seed: " + levelSeed.ToString());
+
+        Random.seed = levelSeed;
+
+        LoadMap();
+    }
+
+    protected void uLink_OnFailedToConnect(uLink.NetworkConnectionError error) {
+        Debug.Log("Failed to connect: " + error);
+    }
+
+    private void LoadMap() {
+        Map map = new MapGenerator().Generate(10, 10);
+
+        GameObject mapGo = UnityUtils.LoadResource<GameObject>("Prefabs/Map", true);
+        mapGo.name = "Map";
+
+        mapGo.GetComponent<MapEntity>().SetMap(map);
+    }
+}
---
+++ UmbraClientUnity/Assets/Code/Network/NetworkServer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using uLink;
+using UnityEngine;
+
+public class NetworkServer : UnityEngine.MonoBehaviour {
+    public int MaxConnections = 2;
+    public int Port = 7100;
+    public int LevelSeed = 1337;
+
+    public GameObject PlayerCreator;
+    public GameObject PlayerOwner;
+    public GameObject PlayerProxy;
+
+    private MapEntity _mapEntity;
+
+    protected void Awake() {
+        uLink.Network.isAuthoritativeServer = true;
+        uLink.Network.InitializeServer(MaxConnections, Port);
+    }
+
+    protected void uLink_OnServerInitialized() {
+        Debug.Log("Server started successfully");
+
+        Random.seed = LevelSeed;
+
+        LoadMap();
+    }
+
+    protected void uLink_OnPlayerApproval(NetworkPlayerApproval approval) {
+        approval.Approve(LevelSeed);
+    }
+
+    protected void uLink_OnPlayerConnected(uLink.NetworkPlayer player) {
+        Debug.Log("Player connected from " + player.ipAddress + ":" + player.port);
+
+        InstantiatePlayer(player);
+    }
+
+    private void LoadMap() {
+        Map map = new MapGenerator().Generate(10, 10);
+
+        GameObject mapGo = UnityUtils.LoadResource<GameObject>("Prefabs/Map", true);
+        mapGo.name = "Map";
+
+        _mapEntity = mapGo.GetComponent<MapEntity>();
+        _mapEntity.SetMap(map);
+    }
+
+    private void InstantiatePlayer(uLink.NetworkPlayer player) {
+        Rect roomBounds = _mapEntity.GetBoundsForCoord(new XY(0, 0));
+
+        Vector2 mapCenter = roomBounds.center;
+        Vector3 startPos = new Vector3(mapCenter.x, GameConfig.BLOCK_SIZE, mapCenter.y);
+
+        uLink.Network.Instantiate(player, PlayerProxy, PlayerOwner, PlayerCreator, startPos, Quaternion.identity, 0, "Villian");
+    }
+}
