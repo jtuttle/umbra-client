dd4789258a7ca1539fe201e3047206b207e9db31
John Tuttle <jtuttle@craw.cc>
2014-02-12 00:12:55 -0500

    server and client setup

--- UmbraMonogame/UmbraClient/UmbraClient.cs
+++ UmbraMonogame/UmbraClient/UmbraClient.cs
@@ -1,12 +1,13 @@
 ﻿#region Using Statements
-using System;
-using System.Collections.Generic;
+using Lidgren.Network;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.GamerServices;
+using System;
+using System.Collections.Generic;
 #endregion
 
 namespace UmbraMonogame {
@@ -14,15 +15,26 @@ namespace UmbraMonogame {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        // TODO: move all this client stuff into a helper class, especially the Update logic
+        NetClient client;
+
         public UmbraClient()
             : base() {
 
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            NetPeerConfiguration config = new NetPeerConfiguration("MyExampleName");
+            config.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+            config.LocalAddress = NetUtility.Resolve("localhost");
+
+            client = new NetClient(config);
         }
 
         protected override void Initialize() {
-        
+            client.Start();
+
+            client.DiscoverLocalPeers(14242);
 
             base.Initialize();
         }
@@ -41,6 +53,27 @@ namespace UmbraMonogame {
             if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            NetIncomingMessage msg;
+
+            while((msg = client.ReadMessage()) != null) {
+                Console.WriteLine("Received " + msg.MessageType + " message from " + msg.SenderEndpoint);
+
+                switch(msg.MessageType) {
+                    case NetIncomingMessageType.DiscoveryResponse:
+                        Console.WriteLine("Found server at " + msg.SenderEndpoint);
+
+                        client.Connect(msg.SenderEndpoint);
+
+                        break;
+                    case NetIncomingMessageType.Data:
+                        Console.WriteLine("Received server data");
+
+                        // process data from server
+
+                        break;
+                    default: break;
+                }
+            }
 
 
             base.Update(gameTime);
@@ -52,6 +85,12 @@ namespace UmbraMonogame {
             
 
             base.Draw(gameTime);
+        }
+
+        protected override void OnExiting(object sender, EventArgs args) {
+            client.Shutdown("bye");
+
+            base.OnExiting(sender, args);
         }
     }
 }
--- UmbraMonogame/UmbraServer/Program.cs
+++ UmbraMonogame/UmbraServer/Program.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using Lidgren.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace UmbraServer {
     class Program {
         static void Main(string[] args) {
+            // configure and start server
+            NetPeerConfiguration config = new NetPeerConfiguration("MyExampleName");
+            config.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+            config.LocalAddress = NetUtility.Resolve("localhost");
+            config.Port = 14242;
 
+            NetServer server = new NetServer(config);
+            server.Start();
+
+            Console.WriteLine("Server running on " + config.LocalAddress + ":" + server.Port);
+
+            // schedule initial sending of client updates
+            float updatesPerSecond = 30.0f;
+			double nextSendUpdates = NetTime.Now;
+
+            // run until escape key is pressed
+            while(!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.Escape) {
+                NetIncomingMessage msg;
+
+                // read messages from server
+                while((msg = server.ReadMessage()) != null) {
+                    Console.WriteLine("Received " + msg.MessageType + " message from " + msg.SenderEndpoint);
+
+                    switch(msg.MessageType) {
+                        case NetIncomingMessageType.DiscoveryRequest:
+                            server.SendDiscoveryResponse(null, msg.SenderEndpoint);
+                            break;
+                        case NetIncomingMessageType.StatusChanged:
+                            NetConnectionStatus status = (NetConnectionStatus)msg.ReadByte();
+
+                            long id = msg.SenderConnection.RemoteUniqueIdentifier;
+                            Console.WriteLine("Player " + NetUtility.ToHexString(id) + " " + status.ToString());
+
+                            switch(status) {
+                                case NetConnectionStatus.Connected:
+                                    // instantiate client, etc
+
+                                    break;
+                            }
+
+                            break;
+                        case NetIncomingMessageType.Data:
+                            // process client data
+
+                            break;
+                        case NetIncomingMessageType.DebugMessage:
+                        case NetIncomingMessageType.VerboseDebugMessage:
+                        case NetIncomingMessageType.WarningMessage:
+                        case NetIncomingMessageType.ErrorMessage:
+                            Console.WriteLine(msg.ReadString());
+                            break;
+                    }
+                }
+
+				if(NetTime.Now > nextSendUpdates) {
+                    // send update messages to clients
+				    foreach (NetConnection player in server.Connections) {
+						foreach (NetConnection otherPlayer in server.Connections) {
+							NetOutgoingMessage om = server.CreateMessage();
+
+                            // write message contents
+
+							server.SendMessage(om, player, NetDeliveryMethod.Unreliable);
+						}
+					}
+
+					// schedule next update
+                    nextSendUpdates += (1.0 / updatesPerSecond);
+				}
+			
+                // sleep to allow other processes to run smoothly
+                Thread.Sleep(1);
+            }
+
+            server.Shutdown("app exiting");
         }
     }
 }
