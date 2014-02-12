114e8bdc8ebbf68873776e8262322d4d68db6c9a
John Tuttle <jtuttle@craw.cc>
2014-02-11 23:40:12 -0500

    removed the copious but unnecessary comments

--- UmbraMonogame/UmbraClient/Program.cs
+++ UmbraMonogame/UmbraClient/Program.cs
@@ -6,14 +6,8 @@ using System.Linq;
 
 namespace UmbraMonogame {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
--- UmbraMonogame/UmbraServer/Program.cs
+++ UmbraMonogame/UmbraServer/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace UmbraServer {
     class Program {
         static void Main(string[] args) {
+
         }
     }
 }
